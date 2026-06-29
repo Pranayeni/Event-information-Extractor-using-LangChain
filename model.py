3ce5f4da512848bc6a04775ea26a63298225cc2a
Pranayeni <pranayeni12@gmail.com>
2026-06-29 14:57:14 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,6 +8,6 @@ def get_llm():
         raise EnvironmentError("Gemini_API_KEY not set in environment variables.")
     return ChatGoogleGenerativeAI(
         model="gemini-2.5-flash-lite",
-        Gemini_API_KEY=api_key,
+        google_api_key=api_key,
         temperature=0
     )
